3a1c427a6a3a98a3504eee3ba2bc97a85ba643ca
Nikita Karetnikov <myipodtouch2014@gmail.com>
2022-10-15 10:42:29 +0300

    correct code

--- app/src/main/java/com/nick/byfilemanagerandsteram/MainActivity.kt
+++ app/src/main/java/com/nick/byfilemanagerandsteram/MainActivity.kt
@@ -1,50 +1,43 @@
 package com.nick.byfilemanagerandsteram
 
-import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Environment
-import android.util.Log
-import android.widget.Button
-import android.widget.EditText
-import android.widget.TextView
 import android.widget.Toast
+import com.nick.byfilemanagerandsteram.databinding.ActivityMainBinding
 import java.io.File
-
 import java.lang.Exception
+
+private val binding: ActivityMainBinding? = null
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-        // Declaring EditText, Buttons and TextViews from the layout file
-        val mEditText = findViewById<EditText>(R.id.edit_text)
-        val mButtonSave = findViewById<Button>(R.id.button1)
-        val mTextView = findViewById<TextView>(R.id.text_view)
+        val mEditText: String = binding.editText.toString()
+        val mButtonSave = binding.button1
 
-        // What happens when Save Button is pressed
         mButtonSave.setOnClickListener {
-            if (mEditText.text.toString().isNotEmpty()) {
-
-                // For First time: Creates a text file and writes string into it
-                // Else: Opens the text file and writes the string
+            if (mEditText.isNotEmpty()) {
                 try {
-                    val data: String = "om namah shivaya"
-                    val path = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS)
+                    val data = "putin huy"
+                    val path =
+                        Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS)
 
-                    val folder = File(path, "urk")
+                    val folder = File(path, "fwe")
                     folder.mkdirs()
 
-                    println(folder.exists()) // u'll get true
-
                     val file = File(folder, "fff.txt")
-                    file.appendText("$data")
-                    Log.d("nick123","work")
+                    file.appendText(data)
 
-                    Toast.makeText(baseContext, "File save on this adress $path!", Toast.LENGTH_LONG)
+                    Toast.makeText(
+                        baseContext,
+                        "File save on this adress $path!",
+                        Toast.LENGTH_LONG
+                    )
                         .show()
-                    Log.d("nick123","Nework")
 
                 } catch (e: Exception) {
                     e.printStackTrace()
@@ -53,8 +46,5 @@ class MainActivity : AppCompatActivity() {
                 Toast.makeText(applicationContext, "No input?", Toast.LENGTH_SHORT).show()
             }
         }
-
-
-
-}
     }
+}
